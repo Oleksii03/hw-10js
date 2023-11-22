a899b953f64aea5a8e6d654c4b34bc6f658cc899
Oleksii_Pivniev <102686871+Oleksii03@users.noreply.github.com>
2023-11-22 19:25:49 +0200

    Update searchApi.js

--- src/js/searchApi.js
+++ src/js/searchApi.js
@@ -76,17 +76,20 @@ btnSearchUp.addEventListener('click', onScrollBtnClose);
 function onScrollBtnOpen (e) {
   console.log(scrollY);
 
-  if (scrollY <= 0) {
-    btnSearchUp.classList.remove('is-visible');
-  }
+  // if (scrollY <= 0) {
+  //   btnSearchUp.classList.remove('is-visible');
+  // }
 
-  if (scrollY > 500) {
+  if (scrollY > 700) {
     btnSearchUp.classList.add('is-visible');
     document.removeEventListener('scroll', throttleScroll);
   }
 };
 
 function onScrollBtnClose (e) {
-  btnSearchUp.classList.remove('is-visible');
+  setTimeout(() => {
+    btnSearchUp.classList.remove('is-visible');
+  }, 250);
+
   document.addEventListener('scroll', throttleScroll);
 }
